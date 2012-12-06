b67598ff5cb78f1f6653b6bc4c8b530833707d82
Marco Braak <mbraak@ridethepony.nl>
2012-12-06 11:36:12 +0100

    Use django-assets for building coffeescript

---
+++ fiber/assets.py
@@ -0,0 +1,12 @@
+from django_assets import Bundle, register
+
+
+coffee = Bundle(
+    'fiber/js/util.coffee',
+    'fiber/js/fiber.coffee',
+    'fiber/js/login.coffee',
+    filters='coffeescript',
+    output='fiber/js/fiber.js'
+)
+
+register('fiber_coffee', coffee)
