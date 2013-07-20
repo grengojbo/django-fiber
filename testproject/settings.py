6ac1c5214116269e94329ec063ce8dd8da0d5faa
Oleg Dolya <oleg.dolya@gmail.com>
2013-07-20 15:03:29 +0300

    Added fixtures

--- testproject/settings.py
+++ testproject/settings.py
@@ -12,7 +12,7 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'testproject_db',                # Or path to database file if using sqlite3.
+        'NAME': 'testproject/testproject_db',                # Or path to database file if using sqlite3.
         'USER': '',                              # Not used with sqlite3.
         'PASSWORD': '',                          # Not used with sqlite3.
         'HOST': '',                              # Set to empty string for localhost. Not used with sqlite3.
